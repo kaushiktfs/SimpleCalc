56cc4eef0d305f5f8e6ef451bd737e7fd969569d
kaushiktfs <36615838+kaushiktfs@users.noreply.github.com>
2018-04-03 11:38:32 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,7 +1,7 @@
 $Number = 1
 $Success = D:\a\1\s\success.json
 $Failure = D:\a\1\s\failure.json
-$Destination = D:\a\1\s\Destination.json
+$Destination = D:\a\1\s\application.json
 if($Number -gt 0){
 Copy-Item $Success -Destination $Destination
 }else{
