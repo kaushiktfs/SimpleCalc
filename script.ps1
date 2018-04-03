78fc561f80c339cd8bc06cfc23929bbc9bf9fe46
kaushiktfs <36615838+kaushiktfs@users.noreply.github.com>
2018-04-03 19:41:23 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,7 +2,7 @@ $Number = "%ErrorLevel%"
 $Success = "D:\a\1\s\success.json"
 $Failure = "D:\a\1\s\failure.json"
 $Destination = "D:\a\1\s\application.json"
-if($Number -gt 0){
+if($Number -eq 0){
 Copy-Item $Success -Destination $Destination
 }else{
 Copy-Item $Failure -Destination $Destination
